b930ef91afadfd3e34424bfc41607684fc8ba9a0
Kot Dymok <kot.dymok@gmail.com>
2022-05-12 14:42:13 +0200

    Changed default apv file

--- code/runAPV.sh
+++ code/runAPV.sh
@@ -2,4 +2,4 @@ if [[ ! -f "link.C" ]]; then
   rootcint -f link.C -c -p link.h LinkDef.h
 fi
 # root -b -q -e 'gROOT->ProcessLine(".L link.C"); gROOT->ProcessLine(".L apv.C"); gROOT->ProcessLine("(new apv(\"fe55_run17-data\"))->Loop()")'
-root -b -q -e 'gROOT->ProcessLine(".L link.C"); gROOT->ProcessLine(".L apv.C"); gROOT->ProcessLine("(new apv(\"run1\"))->Loop()")'
+root -b -q -e 'gROOT->ProcessLine(".L link.C"); gROOT->ProcessLine(".L apv.C"); gROOT->ProcessLine("(new apv(\"run56\"))->Loop()")'
