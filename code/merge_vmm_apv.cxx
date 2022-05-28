cb22cb05df4259fccacb8a206b1f004797c5afb8
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2022-05-28 14:59:20 +0200

    Added renormalization of pdo to merging script

--- code/merge_vmm_apv.cxx
+++ code/merge_vmm_apv.cxx
@@ -10,6 +10,22 @@ void merge_vmm_apv(){
   auto hits_apv = apvan->GetCentralHits(1653145627, 1653145627);
   auto vmman = new evBuilder(run_pair.first, "g1_p25_s100-0&60", "map-20220518");
   auto hits_vmm = vmman->GetCentralHits(1653145627, 1653145627);
+  {
+    double pdoMax = 0.0;
+    for(auto &h: hits_vmm)
+      if(h.pdoRelative > pdoMax)
+        pdoMax = h.pdoRelative;
+    for(auto &h: hits_vmm)
+      h.pdoRelative /= pdoMax;          
+  }
+  {
+    double pdoMax = 0.0;
+    for(auto &h: hits_apv)
+      if(h.pdoRelative > pdoMax)
+        pdoMax = h.pdoRelative;
+    for(auto &h: hits_apv)
+      h.pdoRelative /= pdoMax;          
+  }
 
   printf("APV hits:\n");
   for(auto &h: hits_apv) h.print();
