20dd4351f8e48865a2b2ee906f26b4b63647c48f
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2022-09-19 01:34:45 +0300

    Added additional comments

--- code/hitsMapper.cpp
+++ code/hitsMapper.cpp
@@ -6,7 +6,8 @@
 using std::cout, std::endl;
 using std::ifstream, std::ofstream;
 
-int apv_time_from_SRS(int srs1, int srs2)
+// IMPORTANT change: fixSRSTime
+int apv_time_from_SRS(int srs1, int srs2, bool fixSRSTime = true)
 {
     int diff = 0;
     int srsT_period = 16777215;
@@ -20,7 +21,7 @@ int apv_time_from_SRS(int srs1, int srs2)
     }
 
     // Since standard time between SRSTimeStamps is 400038 (400037), but not 400000, correct srsTimeStamp clock time
-    return round(diff * 25.0 * (400000.0/400037.0) / 1000.0);
+    return round(diff * 25.0 * (fixSRSTime ? 400000.0/400037.0 : 1.0) / 1000.0);
 }
 
 int vmmRemoveFirstNPuslers(vector<pair<unsigned long, analysisGeneral::mm2CenterHitParameters>> &hits_vmm_v, int nRemoved = 637){
@@ -513,7 +514,7 @@ void hitsMapper(bool tight = false, bool analyseData = true)
                     }
                     else if (currEvent->hitsX.size() == 0)
                         continue;
-                    else if (!currEvent->trigger) // Important change: passing event without triple scint coincsidence
+                    else if (!currEvent->trigger) // IMPORTANT change: passing event without triple scint coincsidence
                         continue;
  
                     diff_hit = (currEvent->bcid - prevSyncBcid >= 0) ? currEvent->bcid - prevSyncBcid : currEvent->bcid + 4096 - prevSyncBcid;
