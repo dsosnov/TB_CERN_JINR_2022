dfe02022273a5947684d6cf88080f9085c4bf39e
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2022-10-14 16:28:00 +0300

    Removed alternative start option from hitsMapper

--- code/hitsMapper.cpp
+++ code/hitsMapper.cpp
@@ -280,40 +280,6 @@ map<pair<string, string>, pair<int, int>> firstPulserMap = {
     // {{"run_0789", "run380"} // First APV vs VMM dt = -6584644 or -658 pulses *(VMM is earlier than APV)
     {{"run_0788", "run379"}, {350758, 18140}}, // First APV vs VMM dt = 15829384 or 1582 pulses
 };
-// Alternative: first VMM pulser, usiailly about 33ms before first APV
-map<pair<string, string>, pair<int, int>> firstPulserMapAlternative = {
-    {{"run_0832", "run423"}, {179396, 7042}}, // First APV vs VMM dt = 23209318 or 2320 pulses
-    {{"run_0832_cut", "run423_cut"}, {179396, 7042}}, // First APV vs VMM dt = 23209318 or 2320 pulses
-    {{"run_0832_cut10m", "run423_cut10m"}, {179396, 7042}}, // First APV vs VMM dt = 23209318 or 2320 pulses
-    {{"run_0831", "run422"}, {151064, 7675}}, // First APV vs VMM dt = 32988 or 3 pulses
-    {{"run_0830", "run421"}, {239117, 7633}}, // First APV vs VMM dt = 17748110 or 1774 pulses
-    {{"run_0828", "run420"}, {138204, 3669}}, // First APV vs VMM dt = 32934 or 3 pulses
-    // {{"run_0827", "run419"}, {, 7638}}, // First APV vs VMM dt = 18284390 or 1828 pulses // VMM started 30 sec before APV and counts normally
-    {{"run_0826", "run418"}, {79477, 3928}}, // First APV vs VMM dt = 32524 or 3 pulses
-    // {{"run_0825", "run417"} //No vmm pulses
-    // {{"run_0824", "run416"}, {, 3429}}, // First APV vs VMM dt = 1477467 or 147 pulses // VMM started 0.7 sec before APV and counts normally
-    {{"run_0821", "run412"}, {245098, 7662}}, // First APV vs VMM dt = 23988401 or 2398 pulses
-    {{"run_0818", "run411"}, {141090, 3709}}, // First APV vs VMM dt = 26874351 or 2687 pulses
-    // {{"run_0817", "run410"}, {53049, 126}}, // First APV vs VMM dt = 33004 or 3 pulses -- during acces
-    // {{"run_0816", "run409"} // No apv pulses; No vmm pulses
-    {{"run_0815", "run408"}, {237884, 7439}}, // First APV vs VMM dt = 25239911 or 2523 pulses
-    {{"run_0814", "run407"}, {193345, 7572}}, // First APV vs VMM dt = 33027 or 3 pulses ; gaps between first n pulses in APV
-    // {{"run_0813", "run406"} // No apv pulses
-    // {{"run_0812", "run405"} // No apv pulses
-    // {{"run_0809", "run399"} // No apv pulses
-    // {{"run_0808", "run398"} // No apv pulses; No vmm pulses
-    // {{"run_0807", "run397"} // No apv pulses
-    {{"run_0805", "run395"}, {202356, 7800}}, // First APV vs VMM dt = 32394 or 3 pulses
-    {{"run_0802", "run394"}, {217939, 9372}}, // First APV vs VMM dt = 32976 or 3 pulses
-    {{"run_0801", "run393"}, {228878, 11715}}, // First APV vs VMM dt = 33058 or 3 pulses
-    // {{"run_0800", "run392"} // No apv pulses
-    // {{"run_0797", "run388"} // No apv pulses
-    {{"run_0794", "run385"}, {329538, 11716}}, // First APV vs VMM dt = 41550608 or 4155 pulses
-    {{"run_0792", "run383"}, {264602, 11227}}, // First APV vs VMM dt = 18083779 or 1808 pulses
-    // {{"run_0791", "run382"} // First APV vs VMM dt = -5158615 or -515 pulses; *(VMM is earlier than APV)
-    // {{"run_0789", "run380"} // First APV vs VMM dt = -6584644 or -658 pulses *(VMM is earlier than APV)
-    {{"run_0788", "run379"}, {350095, 18140}}, // First APV vs VMM dt = 15829384 or 1582 pulses
-};
 
 constexpr bool PRINT_TO_FILE = true;
 constexpr bool DEBUG_PRINT = false;
@@ -324,7 +290,7 @@ constexpr bool saveTemporaryParameters = true;
 constexpr bool reloopVMMFile = false;
 
 // AlternativeStart -- first VMM selected as first good VMM pulser in case the difference between first good APV and VMM is about 33ms
-void hitsMapper(bool tight = false, bool fixSRSTime = false, int nAll = 1, int n = 0, string runVMM="0832", string runAPV = "423", bool alternativeStart = false, int mergeTimeWindow = 1000)
+void hitsMapper(bool tight = false, bool fixSRSTime = false, int nAll = 1, int n = 0, string runVMM="0832", string runAPV = "423", int mergeTimeWindow = 1000)
 {
     pair<string, string> run_pair = {Form("run_%s", runVMM.c_str()), Form("run%s", runAPV.c_str())};
     if(!firstPulserMap.count(run_pair))
@@ -334,7 +300,7 @@ void hitsMapper(bool tight = false, bool fixSRSTime = false, int nAll = 1, int n
     }
     // pair<string, string> run_pair = {"run_0832_cut", "run423_cut"};
     // pair<int,int> firstSelectedEntries = {180038 + 21, 7042};
-    pair<int,int> firstSelectedEntries = alternativeStart ? firstPulserMapAlternative.at(run_pair) : firstPulserMap.at(run_pair);
+    pair<int,int> firstSelectedEntries = firstPulserMap.at(run_pair);
 
     auto apvan = new apv(run_pair.second);
     apvan->useSyncSignal();
@@ -371,23 +337,22 @@ void hitsMapper(bool tight = false, bool fixSRSTime = false, int nAll = 1, int n
 
     string tightText = tight ? "_tight" : "";
     string fixTimeText = fixSRSTime ? "_timefix" : "";
-    string alternativeText = alternativeStart ? "_alternative" : "";
     string dupText = findBestVMM ? "" : "_firstVMM";
 
     ofstream out_APV;
     if(PRINT_TO_FILE)
-        out_APV.open(TString("../out/APV_hits_maped_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+dupText+alternativeText+numberingText+".txt").Data());
+        out_APV.open(TString("../out/APV_hits_maped_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+dupText+numberingText+".txt").Data());
 
     // ofstream out_VMM;
-    // out_VMM.open(TString("../out/VMM_hits_"+run_pair.first+"_"+run_pair.second+"_after"+tightText+fixTimeText+dupText+alternativeText+numberingText+".txt").Data());
+    // out_VMM.open(TString("../out/VMM_hits_"+run_pair.first+"_"+run_pair.second+"_after"+tightText+fixTimeText+dupText+numberingText+".txt").Data());
 
     ofstream out_VMM_hits;
     if(PRINT_TO_FILE)
-        out_VMM_hits.open(TString("../out/VMM_hits_UNmaped_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+dupText+alternativeText+numberingText+".txt").Data());
+        out_VMM_hits.open(TString("../out/VMM_hits_UNmaped_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+dupText+numberingText+".txt").Data());
 
     int numOfMapped = 0;
 
-    auto out = TFile::Open(TString("../out/mapped_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+dupText+alternativeText+numberingText+".root"), "recreate");
+    auto out = TFile::Open(TString("../out/mapped_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+dupText+numberingText+".root"), "recreate");
 
     static long long eventNumAPV = -1, eventNumVMM = -1;
     static long long deltaT;
@@ -397,7 +362,7 @@ void hitsMapper(bool tight = false, bool fixSRSTime = false, int nAll = 1, int n
     mappedEventNums->Branch("vmm", &eventNumVMM);
     mappedEventNums->Branch("deltaT", &deltaT);
     TFile* mappedEventBackupFile = nullptr;
-    TString mapBackupFileName = TString("../out/mappedEvents_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+dupText+alternativeText+numberingText+"_bak.root");
+    TString mapBackupFileName = TString("../out/mappedEvents_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+dupText+numberingText+"_bak.root");
     // map<long long, pair<long long, int>> mappedEventMap = {};
 
     auto stripsVMM = make_shared<TH1F>("stripsVMM", "stripsVMM", 360, 0, 360);
@@ -473,7 +438,7 @@ void hitsMapper(bool tight = false, bool fixSRSTime = false, int nAll = 1, int n
     /* Try to load variables from previous runs */
     if(saveTemporaryParameters)
     {
-        TString tmpFileName = TString("../out/beforeLastPulserParameters_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+alternativeText+".root");
+        TString tmpFileName = TString("../out/beforeLastPulserParameters_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+".root");
         auto file = TFile::Open(tmpFileName, "read");
         if(file != nullptr)
         {
@@ -1045,7 +1010,7 @@ void hitsMapper(bool tight = false, bool fixSRSTime = false, int nAll = 1, int n
 
     if(saveTemporaryParameters)
     {
-        TString tmpFileName = TString("../out/beforeLastPulserParameters_out_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+dupText+alternativeText+numberingText+".root");
+        TString tmpFileName = TString("../out/beforeLastPulserParameters_out_"+run_pair.first+"_"+run_pair.second+tightText+fixTimeText+dupText+numberingText+".root");
         auto file = TFile::Open(tmpFileName, "recreate");
         auto tree = new TTree("beforeLastPulserParameters", "");
         tree->SetDirectory(file);
