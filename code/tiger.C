05584e29862c395f2f6301379048518836d9ca30
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2022-10-16 14:04:24 +0300

    Added plots per-tiger (as addition to per-detector); resorted histograms in output file

--- code/tiger.C
+++ code/tiger.C
@@ -5,7 +5,7 @@
 #include "TH2F.h"
 
 map<pair<int, int>, string> detectorNames = {
-  {{0, 0}, "scintillator coincidence"},
+  {{0, 0}, "scintillator channel"},
   {{0, 4}, "50us pulser"},
   {{0, 5}, "10ms pulser"},
   {{6, 0}, "SHiP straw"},
@@ -31,22 +31,72 @@ void tiger::Loop(unsigned long n)
     if(detMax.at(s.second.first) < 0 || detMax.at(s.second.first) < s.second.second)
       detMax.at(s.second.first) = s.second.second;
   }
-  printf("Scintillators (det 0): %d-%d\n", detMin.at(0), detMax.at(0));
-  printf("Straws (det 1): %d-%d\n", detMin.at(1), detMax.at(1));
-  for(int i = 0; i < 4; i++)
-    printf("MM %d (det %d): %d-%d\n", i, i+2, detMin.at(i+2), detMax.at(i+2));
-  printf("Additional straws (det 6): %d-%d\n", detMin.at(6), detMax.at(6));
-  printf("Lemo (det 7): %d-%d\n", detMin.at(7), detMax.at(7));
+  {
+    printf("Scintillators (det 0):");
+    if(detMax.at(0) >= 0) printf(" %d-%d", detMin.at(0), detMax.at(0));
+    printf("\n");
+    printf("Straws (det 1):");
+    if(detMax.at(1) >= 0) printf(" %d-%d", detMin.at(1), detMax.at(1));
+    printf("\n");
+    for(int i = 0; i < 4; i++){
+      printf("MM %d (det %d):", i, i+2);
+      if(detMax.at(i+2) >= 0) printf(" %d-%d", detMin.at(i+2), detMax.at(i+2));
+      printf("\n");
+    }
+    printf("Additional straws (det 6):");
+    if(detMax.at(6) >= 0) printf(" %d-%d", detMin.at(6), detMax.at(6));
+    printf("\n");
+    printf("Lemo (det 7):");
+    if(detMax.at(7) >= 0) printf(" %d-%d", detMin.at(7), detMax.at(7));
+    printf("\n");
+  }
 
   TFile *out = new TFile("../out/out_tiger" + ((runFolder == "") ? "" : runFolder + "-") + file + ending, "RECREATE"); // PATH where to save out_*.root file
 
   map<pair<int, int>, TH1F*> hTigerProfile, hTigerProfileMapped;
   out->mkdir("tiger_profiles")->cd();
-  for(auto m: mappedTigers){
-    hTigerProfile.emplace(m, new TH1F(Form("profile_gr%d_t%d", m.first, m.second), Form("%s: profile for gemroc %d tiger %d;channel", file.Data(), m.first, m.second), 64, 0, 64));
-    hTigerProfileMapped.emplace(m, new TH1F(Form("profileMapped_gr%d_t%d", m.first, m.second), Form("%s: profile for mapped channels for gemroc %d tiger %d;channel", file.Data(), m.first, m.second), 64, 0, 64));
-  }
+  for(auto &gr: {0})
+    for(int t = 0; t < 8; t++){
+      pair<int, int> m = {gr, t};
+      hTigerProfile.emplace(m, new TH1F(Form("profile_gr%d_t%d", gr, t), Form("%s: profile for gemroc %d tiger %d;channel", file.Data(), gr, t), 64, 0, 64));
+      hTigerProfileMapped.emplace(m, new TH1F(Form("profileMapped_gr%d_t%d", gr, t), Form("%s: profile for mapped channels for gemroc %d tiger %d;channel", file.Data(), gr, t), 64, 0, 64));
+    }
   out->cd();
+  map<pair<int, int>, TH2F*> hTigerChargeToT, hTigerChargeSH;
+  map<pair<int, int>, TH2F*> hTigerTimeFine, hTigerFullTime;
+  map<pair<int, int>, TH2F*> hTigertCoarse, hTigereCoarse, hTigertFine, hTigereFine;
+  map<tuple<int, int, int>, TH2F*> hTigerChargePerTime, hTigereFinePerTime;
+  map<tuple<int, int, int>, TH1F*> hTigerFullTimePerChannel;
+  for(auto &gr: {0}){
+    auto grD = out->mkdir(Form("gemroc_%d", gr));
+    grD->cd();
+    for(int t = 0; t < 8; t++){
+      grD->mkdir(Form("tiger_%d", t))->cd();
+      pair<int, int> m = {gr, t};
+      hTigerChargeToT.emplace(m, new TH2F(Form("charge_tot_gr%d_t%d", gr, t), Form("%s: charge (Time over Threshold mode) for gemroc %d tiger %d;channel;charge", file.Data(), gr, t),
+                                          64, 0, 64, 1025, 0, 1025));
+      hTigerChargeSH.emplace(m, new TH2F(Form("charge_sh_gr%d_t%d", gr, t), Form("%s: charge (Sample and Hold mode) for gemroc %d tiger %d;channel;charge", file.Data(), gr, t),
+                                         64, 0, 64, 1025, 0, 1025));
+      hTigerTimeFine.emplace(m, new TH2F(Form("timeFine_gr%d_t%d", gr, t), Form("%s: timeFine for gemroc %d tiger %d;channel;time, ns", file.Data(), gr, t), 64, 0, 64, 4096, 0, 409600));
+      hTigertCoarse.emplace(m, new TH2F(Form("tCoarse_gr%d_t%d", gr, t), Form("%s: tCoarse for gemroc %d tiger %d;channel;tCoarse", file.Data(), gr, t), 64, 0, 64, 65536, 0, 65536));
+      hTigereCoarse.emplace(m, new TH2F(Form("eCoarse_gr%d_t%d", gr, t), Form("%s: eCoarse for gemroc %d tiger %d;channel;eCoarse", file.Data(), gr, t), 64, 0, 64, 1024, 0, 1024));
+      hTigertFine.emplace(m, new TH2F(Form("tFine_gr%d_t%d", gr, t), Form("%s: eFine for gemroc %d tiger %d;tFine", file.Data(), gr, t), 64, 0, 64, 1024, 0, 1024));
+      hTigereFine.emplace(m, new TH2F(Form("eFine_gr%d_t%d", gr, t), Form("%s: eFine for gemroc %d tiger %d;channel;eFine", file.Data(), gr, t), 64, 0, 64, 1024, 0, 1024));
+      hTigerFullTime.emplace(m, new TH2F(Form("fullTime_gr%d_t%d", gr, t), Form("%s: fullTime for gemroc %d tiger %d;channel;time, s", file.Data(), gr, t), 64, 0, 64, 6000, 0, 60));
+      for(auto j = 0; j < 64; j++){
+        hTigerChargePerTime.emplace(make_tuple(gr, t, j),
+                                    new TH2F(Form("ChargePerTime_gr%d_t%d_ch%d", gr, t, j),
+                                             Form("%s: Charge for gemroc %d tiger %d channel %d;full time, s; charge%s", file.Data(), gr, t, j, ((energyMode == TigerEnergyMode::SampleAndHold) ? " = 1024 - eFine": "")),
+                                             6000, 0, 60, 512, 0, 1024));
+        hTigereFinePerTime.emplace(make_tuple(gr, t, j),
+                                   new TH2F(Form("eFinePerTime_gr%d_t%d_ch%d", gr, t, j), Form("%s: eFine for gemroc %d tiger %d channel %d;full time, s; eFine", file.Data(), gr, t, j), 6000, 0, 60, 512, 0, 1024));
+        hTigerFullTimePerChannel.emplace(make_tuple(gr, t, j),
+                                         new TH1F(Form("FullTimePerChannel_gr%d_t%d_ch%d", gr, t, j), Form("%s: fullTime for gemroc %d tiger %d channel %d;time, s", file.Data(), gr, t, j), 6000, 0, 60));
+      }
+      grD->cd();
+    }
+    out->cd();
+  }
 
   auto straw_vs_sci = new TH1F("straw_vs_sci", Form("%s: straw vs scint;#Deltat, ns", file.Data()), 1000, -500, 500);
   vector<TH1F*> straw_vs_mm, mm_vs_sci;
@@ -62,51 +112,55 @@ void tiger::Loop(unsigned long n)
   }
 
   vector<TH1F*> hprofile;
-  vector<TH2F*> hchargeToT, hchargeSH;
-  vector<TH2F*> htimeFine, hFullTime;
+  vector<TH2F*> hChargeToT, hChargeSH;
+  vector<TH2F*> hTimeFine, hFullTime;
   vector<TH2F*> htCoarse, heCoarse, htFine, heFine;
   vector<TH2F*> htCoarse10bit;
   map<int, TH2F*> hDeltaTPrev;
   map<pair<int, int>, TH2F*> hDeltaTPrevPerCharge, hChargePerTime, heFinePerTime;
   map<pair<int, int>, TH1F*> hFullTimePerChannel;
   for(auto i = 0; i < nDetectorTypes; i++){
-    out->mkdir(Form("det%d", i))->cd();
+    auto d = out->mkdir(Form("det%d", i));
+    d->cd();
     hprofile.push_back(new TH1F(Form("profile_det%d", i), Form("%s: profile for detector %d;channel", file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1));
-    hchargeToT.push_back(new TH2F(Form("charge_tot_det%d", i), Form("%s: charge (Time over Threshold mode) for detector %d;channel;charge",
-                                                                    file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 1025, 0, 1025));
-    hchargeSH.push_back(new TH2F(Form("charge_sh_det%d", i), Form("%s: charge (Sample and Hold mode) for detector %d;channel;charge",
-                                                                  file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 1025, 0, 1025));
-    htimeFine.push_back(new TH2F(Form("timeFine_det%d", i), Form("%s: timeFine for detector %d;channel;time, ns", file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 4096, 0, 409600));
-
+    hChargeToT.push_back(new TH2F(Form("charge_tot_det%d", i), Form("%s: charge (Time over Threshold mode) for detector %d;channel;charge", file.Data(), i),
+                                  detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 1025, 0, 1025));
+    hChargeSH.push_back(new TH2F(Form("charge_sh_det%d", i), Form("%s: charge (Sample and Hold mode) for detector %d;channel;charge", file.Data(), i),
+                                 detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 1025, 0, 1025));
+    hTimeFine.push_back(new TH2F(Form("timeFine_det%d", i), Form("%s: timeFine for detector %d;channel;time, ns", file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 4096, 0, 409600));
     htCoarse.push_back(new TH2F(Form("tCoarse_det%d", i), Form("%s: tCoarse for detector %d;channel;tCoarse", file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 65536, 0, 65536));
     heCoarse.push_back(new TH2F(Form("eCoarse_det%d", i), Form("%s: eCoarse for detector %d;channel;eCoarse", file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 1024, 0, 1024));
     htFine.push_back(new TH2F(Form("tFine_det%d", i), Form("%s: eFine for detector %d;channel;tFine", file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 1024, 0, 1024));
     heFine.push_back(new TH2F(Form("eFine_det%d", i), Form("%s: eFine for detector %d;channel;eFine", file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 1024, 0, 1024));
-
-    htCoarse10bit.push_back(new TH2F(Form("tCoarse10bit_det%d", i), Form("%s: last 10 bit of tCoarse for detector %d;channel;tCoarse %% 0x400",
-                                                                         file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 1024, 0, 1024));
-
+    htCoarse10bit.push_back(new TH2F(Form("tCoarse10bit_det%d", i), Form("%s: last 10 bit of tCoarse for detector %d;channel;tCoarse %% 0x400", file.Data(), i),
+                                     detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 1024, 0, 1024));
     hFullTime.push_back(new TH2F(Form("fullTime_det%d", i), Form("%s: fullTime for detector %d;channel;time, s", file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 6000, 0, 60));
 
-    if(i == 0 || i == 7)
-      hDeltaTPrev.emplace(i, new TH2F(Form("DeltaTPrev_det%d", i), Form("%s: #Delta T for detector %d;channel;#Delta T, #mus", file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 10000, 0, 100000));
-
-    if(i == 0 || i == 7)
-    for(auto j = detMin.at(i); j <= detMax.at(i); j++)
-      hDeltaTPrevPerCharge.emplace(make_pair(i, j),
-                                  new TH2F(Form("DeltaTPrevPerCharge_det%d_ch%d", i, j), Form("%s: #Delta T for detector %d, channel %d;#Delta T, #mus; charge", file.Data(), i, j), 5000, 0, 50000, 512, 0, 1024));
-    
-    for(auto j = detMin.at(i); j <= detMax.at(i); j++){
-      hChargePerTime.emplace(make_pair(i, j),
-                             new TH2F(Form("ChargePerTime_det%d_ch%d", i, j),
-                                      Form("%s: Charge for detector %d, channel %d;full time, s; charge%s", file.Data(), i, j, ((energyMode == TigerEnergyMode::SampleAndHold) ? " = 1024 - eFine": "")),
-                                      6000, 0, 60, 512, 0, 1024));
-      hFullTimePerChannel.emplace(make_pair(i, j),
-                                  new TH1F(Form("hFullTimePerChannel_det%d_ch%d", i, j), Form("%s: fullTime for detector %d channel %d;time, s", file.Data(), i, j), 6000, 0, 60));
-      heFinePerTime.emplace(make_pair(i, j),
-                                  new TH2F(Form("eFinePerTime_det%d_ch%d", i, j), Form("%s: eFine for detector %d, channel %d;full time, s; eFine", file.Data(), i, j), 6000, 0, 60, 512, 0, 1024));
+    if(detMax.at(i) >= 0){
+      for(auto j = detMin.at(i); j <= detMax.at(i); j++){
+        hChargePerTime.emplace(make_pair(i, j),
+                               new TH2F(Form("ChargePerTime_det%d_ch%d", i, j),
+                                        Form("%s: Charge for detector %d, channel %d;full time, s; charge%s", file.Data(), i, j, ((energyMode == TigerEnergyMode::SampleAndHold) ? " = 1024 - eFine": "")),
+                                        6000, 0, 60, 512, 0, 1024));
+        heFinePerTime.emplace(make_pair(i, j),
+                              new TH2F(Form("eFinePerTime_det%d_ch%d", i, j), Form("%s: eFine for detector %d, channel %d%s;full time, s; eFine", file.Data(), i, j,
+                                                                                   (detectorNames.count({i,j}) ? (string() + " (" + detectorNames.at({i,j}) + ")").c_str() : "")),
+                                       6000, 0, 60, 512, 0, 1024));
+        hFullTimePerChannel.emplace(make_pair(i, j),
+                                    new TH1F(Form("FullTimePerChannel_det%d_ch%d", i, j), Form("%s: fullTime for detector %d channel %d;time, s", file.Data(), i, j), 6000, 0, 60));
+      }
     }
-    
+    if(i == 0 || i == 7){
+      if(detMax.at(i) >= 0){
+        d->mkdir("deltaT")->cd();
+        hDeltaTPrev.emplace(i, new TH2F(Form("DeltaTPrev_det%d", i), Form("%s: #Delta T for detector %d;channel;#Delta T, #mus", file.Data(), i), detMax.at(i) - detMin.at(i) + 1, detMin.at(i), detMax.at(i) + 1, 10000, 0, 100000));
+        for(auto j = detMin.at(i); j <= detMax.at(i); j++){
+          hDeltaTPrevPerCharge.emplace(make_pair(i, j),
+                                       new TH2F(Form("DeltaTPrevPerCharge_det%d_ch%d", i, j), Form("%s: #Delta T for detector %d, channel %d;#Delta T, #mus; charge", file.Data(), i, j), 5000, 0, 50000, 512, 0, 1024));
+        }
+        d->cd();
+      }
+    }    
     out->cd();
   }
   map<pair<int, int>, tigerHitTL> prevHit;
@@ -202,16 +256,27 @@ void tiger::Loop(unsigned long n)
         }
       }
     }
-    if(hTigerProfile.count({hitMain.gemrocID, hitMain.tigerID})){
+    { // per-tiger histograms
       hTigerProfile.at({hitMain.gemrocID, hitMain.tigerID})->Fill(hitMain.channelID);
       if(fchD >=0)
         hTigerProfileMapped.at({hitMain.gemrocID, hitMain.tigerID})->Fill(hitMain.channelID);
+      hTigerChargeToT.at({hitMain.gemrocID, hitMain.tigerID})->Fill(hitMain.channelID, hitMain.chargeToT());
+      hTigerChargeSH.at({hitMain.gemrocID, hitMain.tigerID})->Fill(hitMain.channelID, hitMain.chargeSH());
+      hTigerTimeFine.at({hitMain.gemrocID, hitMain.tigerID})->Fill(hitMain.channelID, hitMain.timeFine());
+      hTigertCoarse.at({hitMain.gemrocID, hitMain.tigerID})->Fill(hitMain.channelID, hitMain.tCoarse);
+      hTigereCoarse.at({hitMain.gemrocID, hitMain.tigerID})->Fill(hitMain.channelID, hitMain.eCoarse);
+      hTigertFine.at({hitMain.gemrocID, hitMain.tigerID})->Fill(hitMain.channelID, hitMain.tFine);
+      hTigereFine.at({hitMain.gemrocID, hitMain.tigerID})->Fill(hitMain.channelID, hitMain.eFine);
+      hTigerFullTime.at({hitMain.gemrocID, hitMain.tigerID})->Fill(hitMain.channelID, timeDifferenceFineNS(hitMain, hitFirst) * 1E-9);
+      hTigerChargePerTime.at({hitMain.gemrocID, hitMain.tigerID, hitMain.channelID})->Fill(timeDifferenceFineNS(hitMain, hitFirst) * 1E-9, hitMain.charge(energyMode));
+      hTigereFinePerTime.at({hitMain.gemrocID, hitMain.tigerID, hitMain.channelID})->Fill(timeDifferenceFineNS(hitMain, hitFirst) * 1E-9, hitMain.eFine);        
+      hTigerFullTimePerChannel.at({hitMain.gemrocID, hitMain.tigerID, hitMain.channelID})->Fill(timeDifferenceFineNS(hitMain, hitFirst) * 1E-9);
     }
-    if (fchD >=0 && fchD < nDetectorTypes){
+    if (fchD >=0 && fchD < nDetectorTypes){ // per-detector histograms
       hprofile.at(fchD)->Fill(fchM);
-      hchargeToT.at(fchD)->Fill(fchM, hitMain.chargeToT());
-      hchargeSH.at(fchD)->Fill(fchM, hitMain.chargeSH());
-      htimeFine.at(fchD)->Fill(fchM, hitMain.timeFine());
+      hChargeToT.at(fchD)->Fill(fchM, hitMain.chargeToT());
+      hChargeSH.at(fchD)->Fill(fchM, hitMain.chargeSH());
+      hTimeFine.at(fchD)->Fill(fchM, hitMain.timeFine());
       htCoarse.at(fchD)->Fill(fchM, hitMain.tCoarse);
       heCoarse.at(fchD)->Fill(fchM, hitMain.eCoarse);
       htFine.at(fchD)->Fill(fchM, hitMain.tFine);
@@ -219,6 +284,8 @@ void tiger::Loop(unsigned long n)
       htCoarse10bit.at(fchD)->Fill(fchM, hitMain.tCoarse%0x400);
       hFullTime.at(fchD)->Fill(fchM, timeDifferenceFineNS(hitMain, hitFirst) * 1E-9);
       hFullTimePerChannel.at(fchMapped)->Fill(timeDifferenceFineNS(hitMain, hitFirst) * 1E-9);
+      hChargePerTime.at(fchMapped)->Fill(timeDifferenceFineNS(hitMain, hitFirst) * 1E-9, hitMain.charge(energyMode));
+      heFinePerTime.at(fchMapped)->Fill(timeDifferenceFineNS(hitMain, hitFirst) * 1E-9, hitMain.eFine);        
       if(!prevHit.count(fchMapped)){
         if(fchD == 0 || fchD == 7){
           printf("First hit in detector %d, channel %d (time since start: %lld us) : ", fchD, fchM, static_cast<long long>(timeDifferenceFineNS(hitMain, hitFirst) * 1E-3));
@@ -233,11 +300,10 @@ void tiger::Loop(unsigned long n)
           hDeltaTPrev.at(fchD)->Fill(fchM, timeDifferenceFineNS(hitMain, prevHit.at(fchMapped)) * 1E-3);
         if(hDeltaTPrevPerCharge.count(fchMapped))
           hDeltaTPrevPerCharge.at(fchMapped)->Fill(timeDifferenceFineNS(hitMain, prevHit.at(fchMapped)) * 1E-3, hitMain.charge(energyMode));
-        hChargePerTime.at(fchMapped)->Fill(timeDifferenceFineNS(hitMain, hitFirst) * 1E-9, hitMain.charge(energyMode));
-        heFinePerTime.at(fchMapped)->Fill(timeDifferenceFineNS(hitMain, hitFirst) * 1E-9, hitMain.eFine);        
         prevHit[fchMapped] = hitMain;
       }
     }
+
     if(fchD == 0){ // Scintillator && clocks
       if(fchM == 4){
         if(prevHit.count({0, 5}))
