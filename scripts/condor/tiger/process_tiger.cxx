0966a688994e3e00d2392a148b49d07635b5f497
Dmitry Sosnov <25247372+dsosnov@users.noreply.github.com>
2022-12-03 22:57:01 +0100

    Corrected process_tiger script

--- scripts/condor/tiger/process_tiger.cxx
+++ scripts/condor/tiger/process_tiger.cxx
@@ -1,7 +1,8 @@
 #include "tiger.C"
+#include <vector>
 
-void process_tiger(string filename, string directory, string mapFile, string calibFile){
-  auto t = new tiger(filename.c_str(), directory.c_str(), mapFile.c_str(), calibFile.c_str());
+void process_tiger(string filename, string directory, string mapFile, string calibFile, std::vector<short> eModes){
+  auto t = new tiger(filename.c_str(), directory.c_str(), mapFile.c_str(), calibFile.c_str(), eModes);
   t->Loop();
   delete t;
 }
