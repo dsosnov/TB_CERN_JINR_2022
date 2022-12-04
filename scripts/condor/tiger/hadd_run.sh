8a61d18fe8e3ee386283a78a138a2c39b8aa2f07
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2022-12-04 19:40:30 +0300

    Added parallele execution of hadd in condor scripts

--- scripts/condor/tiger/hadd_run.sh
+++ scripts/condor/tiger/hadd_run.sh
@@ -3,5 +3,5 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
 fullpath=$2
 folder=$1
 pushd "$fullpath"
-hadd -f "${folder}-merged.root" "${folder}/out_tiger_"*".root"
+hadd -j -f "${folder}-merged.root" "${folder}/out_tiger_"*".root"
 popd
